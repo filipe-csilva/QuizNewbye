09d2fbc386acbbb5cc2827601a55acd91e05d628
Filipe Silva <81836461+filipe-csilva@users.noreply.github.com>
2024-03-08 20:00:24 -0300

    Adicionar arquivos de projeto.

---
+++ QuizNewbye/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace QuizNewbye
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
